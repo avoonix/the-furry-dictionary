2755aac506c048ab761e6a1cc75bf5d126c3c659
Avoo <git@avoonix.com>
2022-05-26 13:49:39 +0200

    fix

--- scripts/screenshots.js
+++ scripts/screenshots.js
@@ -1,9 +1,14 @@
 //@ts-check
 import { chromium } from 'playwright';
-import { readdir } from "fs/promises"
+import { readdir } from 'fs/promises';
+import { createServer } from 'http-server';
 
 (async () => {
-	const browser = await chromium.launch({headless: true});
+	const server = createServer({ root: 'build' });
+	server.listen(3000);
+
+	const browser = await chromium.launch({ headless: true });
+	let done = 0;
 
 	const takeScreenshotOfTerm = async (/** @type {string} */ term) => {
 		const page = await browser.newPage({
@@ -17,16 +22,20 @@ import { readdir } from "fs/promises"
 		});
 		page.screenshot({});
 		await page.locator('.prose').screenshot({ path: `./build/terms/${term}.png`, scale: 'device' });
-        await page.close();
+		await page.close();
+		done++;
+		console.log(`screenshots taken: ${done}`);
 	};
 
-    const promises = [];
-    const entries = await readdir("./definitions")
-    for( const entry of entries) {
-        if(!entry.endsWith(".md")) continue;
-	    promises.push(takeScreenshotOfTerm(entry.replace(/\.md/,"")));
-    }
-    await Promise.allSettled(promises);
+	const promises = [];
+	const entries = await readdir('./definitions');
+	for (const entry of entries) {
+		if (!entry.endsWith('.md')) continue;
+		promises.push(takeScreenshotOfTerm(entry.replace(/\.md/, '')));
+	}
+	await Promise.allSettled(promises);
 
 	await browser.close();
+	server.close();
+	process.exit(0);
 })();
