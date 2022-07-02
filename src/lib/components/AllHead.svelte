360b6cfc142e09c0b68a381f95ceb7f97f086f39
Avoo <git@avoonix.com>
2022-07-02 10:59:08 +0200

    list all definitions

---
+++ src/lib/components/AllHead.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { canonicalUrl } from '$lib/seo';
+	import Meta from './Meta.svelte';
+
+	$: url = canonicalUrl('all');
+</script>
+
+<Meta title="All Definitions" {url} description="All Definitions" />
+
+<svelte:head>
+	<meta name="robots" content="noindex" />
+</svelte:head>
---
+++ src/routes/all.svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+	import type { Page } from '$lib/definitions';
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch, params }) => {
+		const all: Page[] = await fetch('/definitions.json').then((res) => res.json());
+		return {
+			props: {
+				all
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	import WordList from '$lib/components/WordList.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import AllHead from '$lib/components/AllHead.svelte';
+
+	export let all: Page[];
+</script>
+
+<AllHead />
+
+<Header />
+
+<div class="page-container">
+	<h1 class="text-5xl md:text-6xl xl:text-7xl tracking-tight mb-12 ">All Definitions</h1>
+
+	<WordList words={all} />
+</div>
