98f329754f155652a01a4ee0c4f853bd6585b66a
Avoo <git@avoonix.com>
2022-12-16 13:36:21 +0100

    upgrade deps, sveltekit 1.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
